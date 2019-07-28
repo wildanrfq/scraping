2e3159325c6ee6ff2bfbc96e96548958bdf81e4d
てにりしちみ <33510837+danrfq@users.noreply.github.com>
2019-07-28 10:34:51 +0700

    Add files via upload

--- kamusgaul.py
+++ kamusgaul.py
@@ -4,7 +4,7 @@ def kataHariIni():
 	data=h.bs("https://kitabgaul.com/words")
 	kata=data.find("div",class_="word").text
 	deff=data.find("div",class_="definition").text
-	ex=str(data.find("div",class_="example").span)[6:][:-7].replace("<br/>","\n")
+	ex=str(data.find("div",class_="example").span)[6:][:-7].replace("<br/>","\n").replace("<a href=\"/word/a\">A</a>","A").replace("<a href=\"/word/b\">B</a>","B")
 	h.pj({"kata":kata,"definisi":deff,"contoh":ex})
 	
 def cariKata(q):
@@ -14,11 +14,12 @@ def cariKata(q):
 		for kataa in data.findAll("div",class_="entryDetail"):
 			kata=data.find("div",class_="word").text
 			deff=data.find("div",class_="definition").text
-			ex=str(data.find("div",class_="example").span)[6:][:-7].replace("<br/>","\n")
+			ex=str(data.find("div",class_="example").span)[6:][:-7].replace("<br/>","\n").replace("<a href=\"/word/a\">A</a>","A").replace("<a href=\"/word/b\">B</a>","B")
 			dataa.append({"kata":kata,"definisi":deff,"contoh":ex})
 		if len(dataa) == 1:
 			for x in dataa:h.pj(x)
 		else:h.pj(dataa)
 	else:h.pj({"error":"Kata {} tidak ditemukan.".format(q)})
-	
-cariKata("skuy")
+
+kataHariIni()
+cariKata("anjay")
