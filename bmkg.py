1cef55a2a5bfd8a138832dfe0618b6c5930c0a17
てにりしちみ <33510837+danrfq@users.noreply.github.com>
2019-08-19 15:25:32 +0700

    Update bmkg.py

--- bmkg.py
+++ bmkg.py
@@ -28,7 +28,7 @@ def getInfo():
 
 	saran=infoo[3].text
 
-	shakemap=infoo[4].a.get("href")
+	shakemap=data.find("a",class_="tombol shakemap").get("href")
 
 	h.pj({"title":title,"shakemap":shakemap,"waktu":waktu,"magnitudo":magnitudo,"kedalaman":kedalaman,"koordinat":koordinat,"info":infox,"lokasi":lokasi,"arahan":arahan,"saranBMKG":saran})
 
