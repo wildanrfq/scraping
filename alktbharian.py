0528cb6d76140199e438e79b687ce6e7eb430eb0
てにりしちみ <33510837+danrfq@users.noreply.github.com>
2019-08-28 19:34:31 +0700

    Update alktbharian.py

--- alktbharian.py
+++ alktbharian.py
@@ -1,7 +1,7 @@
 from lib.sn import *
 
 dataz=h.bs('https://www.bible.com/id/verse-of-the-day')
-image = h.shorturl(dataz.find("meta",{"property":"og:image"}).get("content"))
+image = dataz.find("meta",{"property":"og:image"}).get("content")
 isi = dataz.find("meta",{"name":"description"}).get("content")[:-1]
 link=dataz.find("meta", {"property":"og:url"}).get("content")
 ayat=h.bs(link).title.text.split(" - ")[1].split(" | ")[0]
